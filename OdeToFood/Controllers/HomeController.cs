e90292b870cc05fe3b1722d1b7558157b0e30a94
Usama Haq <usamahaq@outlook.com>
2017-07-30 06:13:09 +0500

    4.6 Action Results

--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -1,15 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Mvc;
+using OdeToFood.Model;
 
 namespace OdeToFood.Controllers
 {
-    public class HomeController
+    public class HomeController : Controller
     {
-        public string Index()
+        public IActionResult Index()
         {
-            return "Hello, from the Home Controller...";
+            Resturant model = new Resturant { Id = 1, Name = "The House of Kobe" };
+            return new ObjectResult(model); // Return Object Result : as JSON
         }
     }
 }
---
+++ OdeToFood/Model/Resturant.cs
@@ -0,0 +1,8 @@
+﻿namespace OdeToFood.Model
+{
+    public class Resturant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
