b03c8a3956f0bb8c5aaa99581662d189af73941d
Usama Haq <usamahaq@outlook.com>
2017-08-01 10:05:48 +0500

    8.7 Creating a User

--- OdeToFood/Controllers/AccountController.cs
+++ OdeToFood/Controllers/AccountController.cs
@@ -1,10 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using OdeToFood.Entities;
 using OdeToFood.ViewModels;
+using System.Threading.Tasks;
 
 namespace OdeToFood.Controllers
 {
     public class AccountController : Controller
     {
+        private SignInManager<User> _signInManager;
+        private UserManager<User> _userManager;
+
+        public AccountController(UserManager<User> userManager,
+            SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
         [HttpGet]
         public IActionResult Register()
         {
@@ -13,9 +26,30 @@ namespace OdeToFood.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Register(RegisterUserViewModel model)
+        public async Task<IActionResult> Register(RegisterUserViewModel model)
         {
-            return null;
+            if (ModelState.IsValid)
+            {
+                User user = new Entities.User()
+                {
+                    UserName = model.Username,
+                };
+
+                var creationResult = await _userManager.CreateAsync(user, model.Password);
+                if (creationResult.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction(nameof(HomeController.Index));
+                }
+                else
+                {
+                    foreach (var error in creationResult.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+            return View();
         }
     }
 }
