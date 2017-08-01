8d65ec54a7cbbce607537e0b0a930f1fb673f493
Usama Haq <usamahaq@outlook.com>
2017-08-01 09:55:30 +0500

    8.6 User Registration

---
+++ OdeToFood/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OdeToFood.ViewModels;
+
+namespace OdeToFood.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Register(RegisterUserViewModel model)
+        {
+            return null;
+        }
+    }
+}
---
+++ OdeToFood/ViewModels/RegisteruserViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OdeToFood.ViewModels
+{
+    public class RegisterUserViewModel
+    {
+        [Required, MaxLength(255)]
+        public string Username { get; set; }
+
+        [Required, DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password), Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; }
+    }
+}
